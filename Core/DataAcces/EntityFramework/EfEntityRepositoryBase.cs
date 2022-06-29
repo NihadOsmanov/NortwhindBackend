9742cf65524a4bea7cf819b40ed6616a2a695c56
Nihad Osmanov <nihad.osmanov.no@outlook.com>
2022-06-30 00:53:49 +0400

    Writing a repository base object

---
+++ Core/DataAcces/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,63 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Core.DataAcces.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            //Using - disposable pattern-dir.
+            //Obyektin həyatını sonlandırmasını garbage collectora buraxmırıq. Contxet bahalı bir obyekt olduğu üçün
+            //using blogu bitdiyi zaman yaddaşdan silinməsini təmin edirik.
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updateEntity = context.Entry(entity);
+                updateEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deleteEntity = context.Entry(entity);
+                deleteEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+    }
+}
