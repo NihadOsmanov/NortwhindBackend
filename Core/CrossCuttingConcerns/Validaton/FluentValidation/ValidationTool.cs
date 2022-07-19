98d5b42d05ccf977212e856ed1f2d04fb5453aa4
Nihad Osmanov <nihad.osmanov.no@outlook.com>
2022-07-20 01:23:58 +0400

    Fluent validation added and interception created

---
+++ Core/CrossCuttingConcerns/Validaton/FluentValidation/ValidationTool.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Core.CrossCuttingConcerns.Validaton.FluentValidation
+{
+    public static class ValidationTool
+    {
+        public static void Validation(IValidator validator, object entity)
+        {
+            var result = validator.Validate((IValidationContext)entity);
+            if(!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -8,7 +8,7 @@ namespace Entities.Concrete
         public string ProductName { get; set; }
         public int CategoryId { get; set; }
         public string QuantityPerUnit { get; set; }
-        public string UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public short UnitsInStock { get; set; }
     }
 }
